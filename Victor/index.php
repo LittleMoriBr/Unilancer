ab19ac51f3fcb43b5fd309005b79efe438e1d5b4
brandao1 <viccorin20@gmail.com>
2022-08-12 14:29:06 -0300

    Atualização 12
    Atualização do front-end do site Unilancer
    
    Página Home
    Página Quem Somos
    Página Localização

---
+++ Victor/index.php
@@ -0,0 +1,521 @@
+<!doctype html>
+<html>
+
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Documento sem título</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="styleIndex.css">
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="js/jquery.min.js">
+
+
+</head>
+
+<body class="rodape">
+
+  <script href="js/bootstrap.bundle.js"></script>
+  <link rel="stylesheet" href="js/bootstrap.bundle.js">
+
+
+  <!-- NAV/MENU -->
+
+  <nav id="nave" class="navbar navbar-expand-lg roxotop">
+    <!-- Container wrapper -->
+    <div class="container">
+      <!-- Navbar brand -->
+      <a class="navbar-brand" href="#"><img id="" src="img/amare12.png" alt="MDB Logo" draggable="false" height="60" /></a>
+
+      <!-- Toggle button -->
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
+
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left links -->
+        <ul class="navbar-nav me-3">
+          <li class="nav-item">
+            <b><a class="nav-link black active d-flex align-items-center" aria-current="page" href="#"><i class="fas fa-bars pe-2"></i>Home</a></b>
+          </li>
+        </ul>
+        <ul class="navbar-nav me-3">
+          <li class="nav-item">
+            <b> <a class="nav-link black active d-flex align-items-center" aria-current="page" href="Somos.php"><i class="fas fa-bars pe-2"></i>Sobre</a></b>
+          </li>
+        </ul>
+        <ul class="navbar-nav me-3">
+          <li class="nav-item">
+            <b> <a class="nav-link black active d-flex align-items-center" aria-current="page" href=""><i class="fas fa-bars pe-2"></i>Serviços</a></b>
+          </li>
+        </ul>
+        <ul class="navbar-nav me-3">
+          <li class="nav-item">
+            <b><a class="nav-link black active d-flex align-items-center" aria-current="page" href="Localizacao.php"><i class="fas fa-bars pe-2"></i>Localização</a></b>
+          </li>
+        </ul>
+        </li>
+        </ul>
+
+
+        <!-- Pesquisar Menu -->
+
+        <form class="d-flex align-items-center w-100 form-search">
+          <div class="input-group">
+            <input type="search" class="input-css form-control" placeholder="Pesquisar" aria-label="Search" />
+          </div>
+          <a href="#!" class="text-white"><i class="fas fa-search ps-3"></i></a>
+        </form>
+
+        <!-- IMAGEM, CONFIG, DARK MODE -->
+
+
+      </div>
+
+      <div class="topnav">
+        <ul class="navbar-nav">
+          <li class="nav2">
+            <a href="" class="">
+              <button class="botao-Nav">Login</button>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="topnav">
+        <ul class="navbar-nav ms-0">
+          <li class="nav2">
+            <a href="" class="">
+              <button class="botao-Nav">Cadastrar</button>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    </div>
+  </nav>
+
+  <!-- Navbar FINAL -->
+
+  <!-- MENSAGEM DE ALERTA -->
+
+  <div class="container-fluid backInicio">
+    <div class="container p-5">
+      <div class="row">
+        <div class="col-6">
+          <div class="inicio">
+            <img src="img/homi2.png" alt="">
+          </div>
+        </div>
+        <div class="col-6">
+          <div class="inicio v-100">
+            <h3 class="teste">Conheça a Unilancer</h3>
+            <p class="teste2">Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis quibusdam quod expedita similique perferendis
+              officiis quos eligendi, numquam dolor sequi
+              reprehenderit consequuntur impedit autem ipsa cumque
+              accusamus deleniti, beatae tenetur. <br>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+              Perspiciatis quibusdam quod expedita similique perferendis
+            </p>
+            <button class="botaoInicial">Saiba mais</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>
+
+  <center>
+    <div class="col-sm-12">
+      <div class="alert info">
+        <span class="closebtn">&times;</span>
+        <strong>ALERTA!</strong> O site está sem javascript no momento, volte mais tarde. :)
+      </div>
+    </div>
+    </div>
+  </center>
+
+  <div class="container-fluid p-0 bg2">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-sm-4 mt-5 p-5">
+          <div class="card">
+            <img class="image-card" src="img/secu.png" height="60px" width="60px" alt="">
+            <p class="cart">Pagamento com segurança</p>
+          </div>
+        </div>
+
+        <div class="col-sm-4 mt-5 p-5">
+          <div class="card">
+            <img class="image-card" src="img/pessoa.png" height="60px" width="60px" alt="">
+            <p class="cart">Mais de 100 tipos de serviços</p>
+          </div>
+        </div>
+
+        <div class="col-sm-4 mt-5 p-5">
+          <div class="card">
+            <img class="image-card" src="img/maps.png" height="60px" width="60px" alt="">
+            <p class="cart">Profissionais em todo o país</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <br><br><br>
+
+
+
+
+    <!-- SOBRE EMPRESA -->
+
+    <div class="coluna11">
+      <div class=" bg1">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="textContainer">
+                <h2 class="espacoTitulo teste1">Sobre nós</h2>
+                <p class="teste2">
+                  Somos uma empresa focada em trazer a melhor experiencia no mercado de freelancer,tanto para o usuário quanto para nossos colaborares, utilizamos diversas ferramentas
+                  para criar um ambiente fácil e intuitivo para que todos possam utilizar de nossos trabalhos.
+                </p>
+              </div>
+            </div>
+            <div class="col-6">
+              <div class="Responsive">
+                <div class="img-container">
+                  <img src="img/m6.png" class="" alt="">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="coluna22">
+      <div class="container-fluid bg1">
+        <div class="container">
+          <div class="row">
+            <div class="col-6">
+              <div class="Responsive">
+                <br><br><br>
+                <div class="img-container">
+                  <img src="img/m6.png" class="" alt="">
+                </div>
+              </div>
+            </div>
+            <div class="col-md-6">
+              <div class="textContainer">
+                <h2 class="teste1">Sobre nós</h2>
+                <p class="teste2">
+                  Somos uma empresa focada em trazer a melhor experiencia no mercado de freelancer,tanto para o usuário quanto para nossos colaborares, utilizamos diversas ferramentas
+                  para criar um ambiente fácil e intuitivo para que todos possam utilizar de nossos trabalhos.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <br><br><br>
+
+    <div class="carousel__container">
+      <div class="carousel__item">
+        <h3 class="cartitu">Manutenção</h3>
+        <img src="img/ser1.png" class="carousel__image">
+      </div>
+      <div class="carousel__item">
+        <h3 class="cartitu">Marketing</h3>
+        <img src="img/ser2.png" class="carousel__image">
+      </div>
+      <div class="carousel__item">
+        <h3 class="cartitu">Automação</h3>
+        <img src="img/ser3.png" class="carousel__image">
+      </div>
+    </div>
+
+    <br><br><br>
+
+    <div class="container-fluid bg1">
+      <div class="container">
+        <div class="row">
+          <div class="col-6">
+            <div class="Responsive">
+              <div class="img-container">
+                <img src="img/h2.png" class="" alt="">
+              </div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="textContainer">
+              <h2 class="espacoTitulo teste1">O que fazemos?</h2>
+              <p class="teste2">
+                Disponibilizamos uma plataforma para unir o consumidor com o prestador de serviço,
+                sempre prezamos para a maior segurança do cliente e uma maior disponibilidade de ferramentas para nossos colaboradores.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="container-fluid bg3">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <button class="accordion">Posso achar profissionais de qualquer área?</button>
+          <div class="panel">
+            <p class="pss">Sim! Aqui na Unilancer você tem acesso a todos tipos de profissionais</p>
+          </div>
+
+          <div class="spa"></div>
+
+          <button class="accordion">2</button>
+          <div class="panel">
+            <p class="pss">Lorem ipsum...</p>
+          </div>
+
+          <div class="spa"></div>
+
+
+          <button class="accordion">3</button>
+          <div class="panel">
+            <p class="pss">Lorem ipsum...</p>
+          </div>
+
+          <div class="spa"></div>
+
+
+          <button class="accordion">4</button>
+          <div class="panel">
+            <p class="pss">Lorem ipsum...</p>
+          </div>
+
+          <div class="spa"></div>
+
+
+          <button class="accordion">5</button>
+          <div class="panel">
+            <p class="pss">Lorem ipsum...</p>
+          </div>
+
+          <div class="spa"></div>
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  </div>
+
+
+
+  <!-- js (falha no AJAX) <button>Click to fade in boxes</button><br><br>
+
+<div id="div1" style="width:80px;height:80px;display:none;background-color:red;"></div><br>
+<div id="div2" style="width:80px;height:80px;display:none;background-color:green;"></div><br>
+<div id="div3" style="width:80px;height:80px;display:none;background-color:blue;"></div>
+
+-->
+
+
+
+
+
+  <!-- POSTS TRABALHOS -->
+
+
+  <!-- Modal -->
+
+
+  <!-- MENSAGEM DE AJUDA -->
+
+
+  <!-- <div class="callout">
+        <div class="callout-header">Precisa de ajuda?</div>
+        <span class="closebtn" onclick="this.parentElement.style.display='none';">×</span>
+        <div class="callout-container">
+          <p>Entre em contato com nossa central de atendimento </p><br><br>
+
+          <button onclick="myFunction()" class="cssbuttons-io-button">Entrar em contato
+            <div class="icon">
+              <svg height="24" width="24" viewBox="0 0 24 24">
+                <path d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z" fill="currentColor"></path>
+              </svg>
+            </div> -->
+
+  <!-- TRABALHOS 2 -->
+
+
+  <!-- OFERTAS -->
+
+
+  <!-- RODAPÉ-->
+
+
+
+  <div class="bran roxo">
+    <div class="container-fluid   roxo">
+      <div class="row  pt-5 roxo">
+
+
+        <div class="container-fluid  cinza roxo">
+
+          <div class="cinza">
+
+            <div class="container">
+              <div class="row">
+
+                <div class="col-lg-4 roxo mt-5 centro">
+
+                  <p class="centro"><img src="https://img.icons8.com/ios/48/ff00b4/waypoint-map.png" /></p>
+
+                  <hr class="roxo">
+
+                  <div class="mt-4 roxo">
+                    <h5>Sobre nós</h5>
+                    <h5>Contatos</h5>
+                    <h5>Home</h5>
+                  </div>
+
+                </div>
+
+
+
+
+
+
+                <div class="col-lg-4 roxo mt-5 ">
+
+
+
+                  <p class="centro"><img src="https://img.icons8.com/ios-glyphs/48/ff00b4/search-contacts.png" /></p>
+
+
+
+                  <hr class="roxo">
+
+
+
+
+                  <div class="centro">
+                    <span class="centro">
+                      <img class="m-2" src="https://img.icons8.com/fluency-systems-regular/38/ff00b4/phone.png" />
+                    </span>
+
+
+
+                    <span class="centro">
+                      <img class="m-2" src="https://img.icons8.com/fluency-systems-regular/38/ff00b4/gmail.png" />
+                    </span>
+
+
+
+                    <span class="centro">
+                      <img class="m-2" src="https://img.icons8.com/material-outlined/38/ff00b4/instagram-new--v1.png" />
+                    </span>
+
+
+
+                  </div>
+
+
+
+
+
+
+                </div>
+
+
+
+                <div class="col-lg-4 mt-5 roxo centro">
+
+
+
+                  <p><img src="https://img.icons8.com/external-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto/48/ff00b4/external-personal-card-travel-yogi-aprelliyanto-basic-outline-yogi-aprelliyanto.png" /></p>
+
+
+
+                  <hr class="roxo">
+
+
+
+                  <h5 class="roxo">Trabalhe conosco</h5>
+                  <h5 class="roxo">Entenda como ganhar dinheiro</h5>
+                  <h5 class="roxo">Primeiros passos</h5>
+                </div>
+
+
+
+
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+
+
+<!-- JS -->
+
+<script src="section.js">
+</script>
+
+
+<script>
+  $(document).ready(function() {
+    $("button").click(function() {
+      $("#div1").fadeIn();
+      $("#div2").fadeIn("slow");
+      $("#div3").fadeIn(3000);
+    });
+  });
+</script>
+
+
+
+
+
+<script>
+  var close = document.getElementsByClassName("closebtn");
+  var i;
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.opacity = "0";
+      setTimeout(function() {
+        div.style.display = "none";
+      }, 600);
+    }
+  }
+</script>
+
+<script>
+  function myFunction() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function() {
+      x.className = x.className.replace("show", "");
+    }, 3000);
+  }
+</script>
+
+<script>
+  function myFunction1() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+  }
+</script>
+
+</html>
